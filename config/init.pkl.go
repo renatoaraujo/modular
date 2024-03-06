8e715bc96102c466cc826af1ec11c8f082866e1f
Renato Araujo <renato.r.araujo@gmail.com>
2024-03-06 07:55:56 +0000

    chore: removes leftovers

--- config/init.pkl.go
+++
@@ -1,9 +0,0 @@
-// Code generated from Pkl module `modular.gallery.config`. DO NOT EDIT.
-package config
-
-import "github.com/apple/pkl-go/pkl"
-
-func init() {
-	pkl.RegisterMapping("modular.gallery.config", Config{})
-	pkl.RegisterMapping("modular.gallery.config#PluginDefinition", PluginDefinitionImpl{})
-}
